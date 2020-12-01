fa68e05cd87e9f0b3bfa4756dd1ca2eee96510f6
Shaun Jennings <shaun.sj97@gmail.com>
2020-11-30 20:48:28 -0500

    Set up layout of objects on screen

---
+++ OS_frontend_web/js/main.js
@@ -0,0 +1,14 @@
+
+  anime({
+  targets: '.square',
+  translateX: 250,
+  rotateZ: 360,
+  scale: 3,
+  backgroundColor: 'white',
+  duration: 3000,
+  loop: true
+});
+
+function move_blocks{
+  
+}
